c4d2db2d275d151432fa1044d2fa9c4996713c28
Akash <Akash@192.168.0.117>
2022-02-03 10:04:01 +0530

    Changes utility class for user story 1234

--- ActiTime-Application/src/main/java/Utils/Utility.java
+++ ActiTime-Application/src/main/java/Utils/Utility.java
@@ -3,21 +3,8 @@ package Utils;
 import org.openqa.selenium.WebDriver;
 
 public class Utility {
-	
-//	public static String getDataFromDatsheet(int row , int col)
-//	{
-//		String data ;
-//
-//
-//	}
-//	
-//	public static void saveScrenshot( WebDriver driver123 , int testID )
-//	{
-//		
-//		date
-//		(TakeScreenshot)driver123
-//		
-//		name :    "Test-101 29-01-2022 09 43 01"
-//	}
-	
+
+	public static void test() {
+		System.out.println("HI");
+	}
 }
